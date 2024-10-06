6ce2e659cf5d2385bb857df526d157deaca58820
developer <developer@gmail.com>
2024-10-06 20:19:29 +0200

    add avatar fix

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,11 @@ export default async function HomePage() {
       <div className="flex items-start md:items-center my-8 flex-col md:flex-row">
         <Image
           alt={name}
-          className="rounded-full grayscale"
+          className=" w-[120px] h-[120px] object-cover object-center rounded-full grayscale"
           src={avatar}
           placeholder="blur"
-          width={100}
+          width={120}
+          height={120}
           priority
         />
         <div className="mt-8 md:mt-0 ml-0 md:ml-6 space-y-2 text-neutral-500 dark:text-neutral-400">
